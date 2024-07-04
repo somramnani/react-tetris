526b82478965552b54fd46054a08d76b34fc2e48
somramnani <som.ramnani@gmail.com>
2024-07-03 20:58:10 -0400

    Edit index.css, add StyledTetris.js and rename StyledCell.js

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,5 +1,12 @@
-const Cell = () => {
-  return <div>Cell</div>;
+import { StyledCell } from "./styles/StyledCell";
+import { TETROMINOS } from "./../utils/tetrominos";
+
+const Cell = ({ type }) => {
+  return (
+    <StyledCell type={type} color={TETROMINOS[type].color}>
+      Cell
+    </StyledCell>
+  );
 };
 
 export default Cell;
