f21ba4ba76eeeb351726c58d40f772d6c0321b51
somramnani <som.ramnani@gmail.com>
2024-07-07 12:24:59 -0400

    Add StyledDisplay styled component

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,5 +1,7 @@
-const Display = () => {
-  return <div>Display</div>;
+import { StyledDisplay } from "./styles/StyledDisplay";
+
+const Display = ({ gameOver, text }) => {
+  return <StyledDisplay gameOver={gameOver}>{text} </StyledDisplay>;
 };
 
 export default Display;
