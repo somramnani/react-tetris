5eb995b9fb60ba0ed808f035aff952609dbc0422
somramnani <som.ramnani@gmail.com>
2024-07-07 12:18:18 -0400

    Remove text fro Cell component

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -2,11 +2,7 @@ import { StyledCell } from "./styles/StyledCell";
 import { TETROMINOS } from "./../utils/tetrominos";
 
 const Cell = ({ type }) => {
-  return (
-    <StyledCell type={type} color={TETROMINOS[type].color}>
-      Cell
-    </StyledCell>
-  );
+  return <StyledCell type={type} color={TETROMINOS[type].color}></StyledCell>;
 };
 
 export default Cell;
