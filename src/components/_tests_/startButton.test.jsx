9897a2b4e6cb2adccab8dff339188a28a8bc8e8c
somramnani <som.ramnani@gmail.com>
2024-07-24 18:55:43 -0400

    Add start button component test suite, add snapshots and update codecov.yml

--- src/components/_tests_/startButton.test.jsx
+++ src/components/_tests_/startButton.test.jsx
@@ -1,10 +1,33 @@
-import { render, screen, cleanup } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import StartButton from "../StartButton";
 
-test("should render Start Button component", () => {
-  render(<StartButton />);
-  const startButtonElement = screen.getByTestId("startButton-1");
-  expect(startButtonElement).toBeInTheDocument();
-  expect(startButtonElement).toHaveTextContent("Start Game");
+describe("Start Button Component", () => {
+  let mockCallback;
+
+  afterEach(() => {
+    mockCallback = jest.fn();
+  });
+
+  it("should render Start Button component", () => {
+    render(<StartButton callback={mockCallback} />);
+
+    const startButtonElement = screen.getByTestId("startButton-1");
+
+    expect(startButtonElement).toBeInTheDocument();
+    expect(startButtonElement).toHaveTextContent("Start Game");
+  });
+
+  it("calls the callback when button is clicked", () => {
+    render(<StartButton callback={mockCallback} />);
+
+    fireEvent.click(screen.getByTestId("startButton-1"));
+
+    expect(mockCallback).toHaveBeenCalled();
+  });
+
+  it("should match snapshot", () => {
+    const { asFragment } = render(<StartButton callback={mockCallback} />);
+    expect(asFragment()).toMatchSnapshot();
+  });
 });
